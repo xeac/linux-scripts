4ae917b78d495c51416336881eef32770dad6e7c
mggeorgiev <mggeorgiev@gmail.com>
2019-04-26 08:37:27 +0300

    updated in post-updates click action

--- postUpdate.sh
+++ postUpdate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 echo 'export HISTTIMEFORMAT="%d/%m/%y %T "' >> ~/.bash_profile
