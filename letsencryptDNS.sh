38bbc9b4108ad88dac0b4cb3af1a963b9777768c
Martin Georgiev <mggeorgiev@mggeorgiev.com>
2019-09-18 22:03:45 +0300

    update letsencript script

--- letsencryptDNS.sh
+++ letsencryptDNS.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo /usr/bin/./sudo ./certbot -d martingeorgiev.com -d *.martingeorgiev.com --server https://acme-v02.api.letsencrypt.org/directory --manual --preferred-challenges dns certonly
+sudo certbot certificates
+
+#sudo certbot -d martingeorgiev.com -d *.martingeorgiev.com --server https://acme-v02.api.letsencrypt.org/directory --manual --preferred-challenges dns certonly
+
+#sudo certbot renew
+
+#sudo ./certbot -d martingeorgiev.com -d *.martingeorgiev.com --server https://acme-v02.api.letsencrypt.org/directory --manual --preferred-challenges dns certonly
