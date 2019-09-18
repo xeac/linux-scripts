079c5759c22293624b0ad031da306d83e838fced
Martin Georgiev <mggeorgiev@mggeorgiev.com>
2019-09-19 02:08:13 +0300

    add gitupdate

---
+++ gitUpdate_martingeorgiev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /var/www/martingeorgiev.com/public_html
+git pull origin master
---
+++ gitUpdate_pekarnaudoma.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /var/www/pekarnaudoma.com/public_html
+git pull origin static-single-page
