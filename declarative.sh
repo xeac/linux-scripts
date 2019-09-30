3c8979faf9cceedb3f49f2430767e3022663ad98
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2019-09-30 14:59:10 +0300

    Create declarative.sh

---
+++ declarative.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#This is declarative Bash script. Actions will only be taken if needed.
+
+if [! -f $(pwd)/file-declarative.txt ]; then
+    echo "Declarative" >> file-declarative.txt
+    echo "File Created"
+else
+    echo "No action needed"
+fi
