786cf4462e3326161651d9bfec3c9730c89693b0
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2019-10-15 11:51:04 +0300

    Update all.sh

--- initial_install/all.sh
+++ initial_install/all.sh
@@ -10,3 +10,10 @@ sudo chmod +x update.sh
 sh update.sh
 
 sudo apt install git
+
+#Pull git repository
+mkdir linux_scripts
+cd linux_scripts
+git init
+git remote add origin https://github.com/mggeorgiev/linux-scripts.git
+git pull origin master
