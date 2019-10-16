0d9961741ba03b5bdc2bb3281ab21c12533020e2
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2019-10-16 08:02:42 +0300

    Update microk8s.sh

--- k8s/microk8s.sh
+++ k8s/microk8s.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+#sudo nano /etc/cloud/cloud.cfg
+
+sudo hostnamectl set-hostname microk8s
+hostnamectl
+
 sudo usermod -a -G microk8s georgiem
 
 token=$(microk8s.kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
