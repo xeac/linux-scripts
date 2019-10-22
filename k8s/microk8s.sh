f97184ea11fc725b372e4c1e75125714f006ce6d
Martin Georgiev <mggeorgiev@gmail.com>
2019-10-22 17:55:38 +0000

    Add alliasing to microk8s.sh

--- k8s/microk8s.sh
+++ k8s/microk8s.sh
@@ -5,7 +5,11 @@
 sudo hostnamectl set-hostname microk8s
 hostnamectl
 
-sudo usermod -a -G microk8s georgiem
+sudo usermod -a -G microk8s $(whoami)
 
 token=$(microk8s.kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
 microk8s.kubectl -n kube-system describe secret $token
+
+snap alias microk8s.kubectl kubectl
+#snap unalias kubectl
+
