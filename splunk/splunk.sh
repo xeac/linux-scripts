88441027fd11d832ccdc850fd31dad60c9dd3f74
Martin Georgiev Georgiev <mggeorgiev@gmail.com>
2019-10-23 11:34:58 +0300

    update splunk

--- splunk/splunk.sh
+++ splunk/splunk.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo /opt/splunk/bin/splunk stop
 sudo dpkg -i /shared/Splunk/splunk-*
+#wget -O splunk-8.0.0-1357bef0a7f6-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.0.0&product=splunk&filename=splunk-8.0.0-1357bef0a7f6-linux-2.6-amd64.deb&wget=true'
 sudo /opt/splunk/bin/splunk start --accept-license --answer-yes
