19428ab91c751d712c7278894bfcf8a070c8c19e
Martin Georgiev Georgiev <mggeorgiev@gmail.com>
2019-10-23 11:03:01 +0300

    Delete postUpdate.sh

--- postUpdate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
-gsettings set org.gnome.desktop.interface show-battery-percentage true
-#echo 'export HISTTIMEFORMAT="%d/%m/%y %T "' >> ~/.bash_profile
-echo 'export HISTTIMEFORMAT="%F %T: "' >> ~/.bash_profile
-source ~/.bash_profile
